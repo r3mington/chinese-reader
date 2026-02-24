98c90dca9c1d471c7338300109ace2becdcf949d
Pelta Remi <remixcube@Peltas-MacBook-Air.local>
2026-02-24 14:43:01 +0800

    fix: remove emergency service worker deletion to re-enable offline PWA

--- src/main.jsx
+++ src/main.jsx
@@ -9,21 +9,3 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-// EMERGENCY FIX: Unregister all service workers to fix blank screen
-// PWA temporarily disabled until we can debug the caching issue
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => {
-      console.log('Unregistering service worker:', registration);
-      registration.unregister();
-    });
-  });
-
-  // Also clear all caches
-  caches.keys().then((keys) => {
-    keys.forEach((key) => {
-      console.log('Deleting cache:', key);
-      caches.delete(key);
-    });
-  });
-}
